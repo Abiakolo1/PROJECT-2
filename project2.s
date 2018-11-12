a8bb947c585a55500df3c39253190dd45b6a552e
ABiakolo1 <aghogho.biakolo@bison.howard.edu>
2018-11-12 18:06:21 -0500

    This is the commit for my next 5 lines to recieve input and to load space

--- project2.s
+++ project2.s
@@ -1,5 +1,13 @@
 .data
-    char array: .space 1000
-    new_line: .asciiz "\n"
-    invalid_base_36: .asciiz "Invalid base-36 number."
-    empty_string: .asciiz "Input is Empty"
+    char array: .space 4 #allocates space for characters
+    new_line: .asciiz "\n" #used to seperate 4 characters from extra ones
+    invalid_base_36: .asciiz "Invalid base-36 number." #needed to output if a string is an invalid base-36 number
+    empty_string: .asciiz "Input is Empty" #needed to output if a string is empty
+    too_long_string: .asciiz "Input is too Long." #needed to output if a string is more than the 4 characters that it is meant to output
+.text
+    main:
+        li $v0, 4 #receives input
+        la $a0, char_array #loads buffer into address
+
+
+
