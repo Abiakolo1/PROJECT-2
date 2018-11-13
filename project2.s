43a3dcfca34127537589efc81c3b5ce71c9bcd6b
ABiakolo1 <aghogho.biakolo@bison.howard.edu>
2018-11-13 11:13:35 -0500

    Commit, changed my char array to a string instead since my new way of doing this might be easier, also made a few modifications here and there

--- project2.s
+++ project2.s
@@ -1,18 +1,17 @@
 .data
-    char array: .space 4 #allocates space for characters
+    string_use: .space 2000 #allocates space for characters
     new_line: .asciiz "\n" #used to seperate 4 characters from extra ones
     invalid_base_36: .asciiz "Invalid base-36 number." #needed to output if a string is an invalid base-36 number
     empty_string: .asciiz "Input is Empty" #needed to output if a string is empty
     too_long_string: .asciiz "Input is too Long." #needed to output if a string is more than the 4 characters that it is meant to output
 .text
     main:
-        li $v0, 4 #receives input
-        la $a0, char_array #loads buffer into address
-        li $a1, 1000 #allocates space for string
+        li $v0, 8 #receives input
+        la $a0, string_use #loads buffer into address
         syscall
-        la $a0, new_line #loads new line
-        li $v0, 4 #prints new line for string
-        syscall
+        la $t1, string_use #loads space here
+
+
 
 
 
