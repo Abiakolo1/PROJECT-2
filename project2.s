dd35a439a73ed913b511defcd8427e5f73ddb015
ABiakolo1 <aghogho.biakolo@bison.howard.edu>
2018-11-16 21:23:59 -0500

    This is my first starting point, I'm starting again and organizing my code better

--- project2.s
+++ project2.s
@@ -1,1 +1,6 @@
+.data
+userStr:    .space 2000
+tooLongErrorMsg:    .asciiz "Input is too long." # use this if the length of the string starting from the first letter to the next letter is longer than the input of 4 digits
+wrongCharErrorMsg:    .asciiz "Invalid base-36 number."#we use this for numbers that have spaces inbetween them and numbers and for numbers that have invalid characters such as the @ symbol in them
+emptyErrorMsg: .asciiz "Input is empty." #we use this for numbers where the input is only filled with spaces and if there is a null character, therefore I will need to check if it is a null string
 
